53d323aabf1aabe6da0c9ff0665638e3f95a2c74
John Vandenberg <jayvdb@gmail.com>
2020-09-02 13:09:36 +0700

    hosts: Python 2 kwargs fix

--- dns_cache/hosts.py
+++ dns_cache/hosts.py
@@ -56,11 +56,10 @@ def loads(filename="/etc/hosts"):
 class _DeserializeOnGetCacheBase(object):
     def __init__(
         self,
-        filename=None,
-        serializer=None,
-        deserializer=None,
+        filename,
+        deserializer,
         *args,
-        **kwargs,
+        **kwargs
     ):
         assert filename and deserializer
         super(_DeserializeOnGetCacheBase, self).__init__(*args, **kwargs)
@@ -83,17 +82,14 @@ class _DeserializeOnGetCacheBase(object):
 class HostsCacheBase(_DeserializeOnGetCacheBase, NoExpirationCacheBase):
     def __init__(
         self,
-        filename=None,
-        serializer=None,
-        deserializer=loads,
+        filename,
         *args,
-        **kwargs,
+        **kwargs
     ):
         super(HostsCacheBase, self).__init__(
-            filename=filename,
-            serializer=serializer,
-            deserializer=deserializer,
             *args,
+            filename=filename,
+            deserializer=loads,
             **kwargs)
 
 
